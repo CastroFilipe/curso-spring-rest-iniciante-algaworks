8ff21e50b6f470721ca5540f9f401dc0907990a1
Filipe Castro <filipe.couto.10@gmail.com>
2020-04-28 02:07:32 -0300

    excluir clientes

--- src/main/java/com/filipe/osworks/api/controller/ClienteController.java
+++ src/main/java/com/filipe/osworks/api/controller/ClienteController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -57,7 +58,7 @@ public class ClienteController {
 	}
 	
 	@PutMapping("/{id}")
-	public ResponseEntity<Cliente> atualizar(@PathVariable Integer id, @RequestBody Cliente cliente){
+	public ResponseEntity<Void> atualizar(@PathVariable Integer id, @RequestBody Cliente cliente){
 		if(!clienteRepository.existsById(id)) {
 			return ResponseEntity.notFound().build();
 		}
@@ -65,6 +66,16 @@ public class ClienteController {
 		cliente.setId(id);
 		clienteRepository.save(cliente);
 		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping("{id}")
+	public ResponseEntity<Void> excluir(@PathVariable Integer id){		
+		if(!clienteRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();//not foun se o objeto se não existir na base de dados
+		}
+		
+		clienteRepository.deleteById(id);
 		return ResponseEntity.noContent().build();
 	}
 }

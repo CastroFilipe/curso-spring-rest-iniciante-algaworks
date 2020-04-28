b3d25bbf0092a20d98afccd5841d796c59595ace
Filipe Castro <filipe.couto.10@gmail.com>
2020-04-27 23:20:43 -0300

    Fazendo consulta atraves de interface no repository

--- src/main/java/com/filipe/osworks/api/controller/ClienteController.java
+++ src/main/java/com/filipe/osworks/api/controller/ClienteController.java
@@ -2,22 +2,25 @@ package com.filipe.osworks.api.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.filipe.osworks.domain.model.Cliente;
+import com.filipe.osworks.domain.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {
 	
-	@PersistenceContext //Semelhante ao @Autowired
-	EntityManager manager;
+//	@PersistenceContext //Semelhante ao @Autowired
+//	EntityManager manager;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	@GetMapping("/clientes")
 	public List<Cliente> teste() {
-		return manager.createQuery("from Cliente", Cliente.class).getResultList();
+		//return manager.createQuery("from Cliente", Cliente.class).getResultList();
+		return clienteRepository.findAll();
 	}
 }

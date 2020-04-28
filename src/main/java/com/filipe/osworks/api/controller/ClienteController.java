b2928c8548cffa0587d526c50db1a59aa020179c
Filipe Castro <filipe.couto.10@gmail.com>
2020-04-28 00:18:30 -0300

    Metodos de busca de clientes

--- src/main/java/com/filipe/osworks/api/controller/ClienteController.java
+++ src/main/java/com/filipe/osworks/api/controller/ClienteController.java
@@ -1,26 +1,48 @@
 package com.filipe.osworks.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.filipe.osworks.domain.model.Cliente;
 import com.filipe.osworks.domain.repository.ClienteRepository;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
-	
+
+//	Busca direta com EntityManager
 //	@PersistenceContext //Semelhante ao @Autowired
 //	EntityManager manager;
+	
+//	@GetMapping("/clientes")
+//	public List<Cliente> teste() {
+//		//return manager.createQuery("from Cliente", Cliente.class).getResultList();
+//	}
 	
 	@Autowired
 	private ClienteRepository clienteRepository;
 	
-	@GetMapping("/clientes")
-	public List<Cliente> teste() {
-		//return manager.createQuery("from Cliente", Cliente.class).getResultList();
+	@GetMapping
+	public List<Cliente> listar() {
 		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Integer id) throws Exception {
+		Optional<Cliente> cliente = clienteRepository.findById(id);
+		
+		if(cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+
 	}
 }

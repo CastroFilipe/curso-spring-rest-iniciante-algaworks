ba32abafa489008f2f16f0dafd543bddebe9bab5
Filipe Castro <filipe.couto.10@gmail.com>
2020-04-27 23:17:07 -0300

    Consulta simples e direta com EntityManager

--- src/main/java/com/filipe/osworks/api/controller/ClienteController.java
+++ src/main/java/com/filipe/osworks/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.filipe.osworks.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +13,11 @@ import com.filipe.osworks.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext //Semelhante ao @Autowired
+	EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> teste() {
-		Cliente c1 = new Cliente(1L, "João", "joao@exemplo.com", "1234567");
-		Cliente c2 = new Cliente(2L, "Paula", "paula@exemplo.com", "987456");
-
-		return Arrays.asList(c1, c2);
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 }

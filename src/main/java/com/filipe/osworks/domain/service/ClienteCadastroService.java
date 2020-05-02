fef248a565abbb194198bde97a09545850cf951a
Filipe Castro <filipe.couto.10@gmail.com>
2020-05-02 01:12:54 -0300

    lançando NegocioException para impedir cadastro de email repetido

--- src/main/java/com/filipe/osworks/domain/service/ClienteCadastroService.java
+++ src/main/java/com/filipe/osworks/domain/service/ClienteCadastroService.java
@@ -3,6 +3,7 @@ package com.filipe.osworks.domain.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.filipe.osworks.domain.exception.NegocioException;
 import com.filipe.osworks.domain.model.Cliente;
 import com.filipe.osworks.domain.repository.ClienteRepository;
 
@@ -13,6 +14,14 @@ public class ClienteCadastroService {
 	private ClienteRepository clienteRepository;
 	
 	public Cliente salvar(Cliente cliente) {
+		clienteRepository.findByEmail(cliente.getEmail())
+			.ifPresent((clienteExistente) -> {
+				//se os ids forem diferentes, indica que não é um update de um registro no bd.
+				if(clienteExistente.getId() != cliente.getId()) {
+					throw new NegocioException("Já existe um cliente cadastrado com esse email");
+				}
+			});
+		
 		return clienteRepository.save(cliente);
 	}
 

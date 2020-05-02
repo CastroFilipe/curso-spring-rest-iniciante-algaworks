ebd063989f00008a701cde509e157c7733d39789
Filipe Castro <filipe.couto.10@gmail.com>
2020-05-01 23:04:48 -0300

    captura e tratamento de exceções

---
+++ src/main/java/com/filipe/osworks/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,55 @@
+package com.filipe.osworks.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+/**
+ * A classe fará, de forma global, o tratamento de exceções lançadas nas classes do pacote controller
+ * 
+ * ResponseEntityExceptionHandler classe que contém tratamento genérico e básico para algumas exceções.
+ * */
+
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+	//quando injetado, trará as informações de mensagens presentes no arquivo messages.properties
+	@Autowired
+	private MessageSource messageSource;
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		//lista de campos
+		List<MessageError.Campo> campos = new ArrayList<>();
+		
+		//percorrer por cada erro de validação presente na exceção
+		for (ObjectError error : ex.getBindingResult().getAllErrors()) {
+			String nomeAtributo = ((FieldError)error).getField();
+			
+			campos.add(new MessageError.Campo(nomeAtributo, messageSource.getMessage(error, LocaleContextHolder.getLocale())));
+		}
+		
+		MessageError mensagemDeErro = new MessageError(
+				status.value(),
+				LocalDateTime.now(), 
+				"Campo(s) preenchido(s) de forma incorreta",
+				campos);
+		
+		return super.handleExceptionInternal(ex, mensagemDeErro, headers, status, request);
+	}
+}
---
+++ src/main/java/com/filipe/osworks/api/exceptionhandler/MessageError.java
@@ -0,0 +1,88 @@
+package com.filipe.osworks.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+/**
+ * Classe que representa uma mensagem padrão de erro. 
+ * Utilizada para a exibição de informações no tratamento de exceções capturadas na classe {@link ApiExceptionHandler}
+ * */
+public class MessageError {
+	//status code
+	private Integer status;
+	
+	//informações de data e hora de ocorrência do erro
+	@JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+	private LocalDateTime dataHora;
+	
+	private String titulo;
+	
+	private List<Campo> campos;
+	
+	//classe interna que representará cada campo inválido.
+	public static class Campo {
+		private String atributo;
+		private String mensagem;
+		
+		public Campo(String atributo, String mensagem) {
+			super();
+			this.atributo = atributo;
+			this.mensagem = mensagem;
+		}
+		
+		public String getAtributo() {
+			return atributo;
+		}
+
+		public void setAtributo(String atributo) {
+			this.atributo = atributo;
+		}
+
+		public String getMensagem() {
+			return mensagem;
+		}	
+	}
+
+	public MessageError(Integer status, LocalDateTime dataHora, String titulo, List<Campo> campos) {
+		super();
+		this.status = status;
+		this.dataHora = dataHora;
+		this.titulo = titulo;
+		this.campos = campos;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public LocalDateTime getDataHora() {
+		return dataHora;
+	}
+
+	public void setDataHora(LocalDateTime dataHora) {
+		this.dataHora = dataHora;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public List<Campo> getCampos() {
+		return campos;
+	}
+
+	public void setCampos(List<Campo> campos) {
+		this.campos = campos;
+	}
+	
+}

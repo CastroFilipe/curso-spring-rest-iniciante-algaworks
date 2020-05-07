7a32fb4a289edbb5f0c94db1ee9c0a0ce3ec6f6e
Filipe Castro <filipe.couto.10@gmail.com>
2020-05-07 01:02:35 -0300

    busca de ordens de serviço por id

--- src/main/java/com/filipe/osworks/api/controller/OrdemServicoController.java
+++ src/main/java/com/filipe/osworks/api/controller/OrdemServicoController.java
@@ -1,12 +1,15 @@
 package com.filipe.osworks.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,5 +41,14 @@ public class OrdemServicoController {
 		return ordemServicoGestaoService.criar(ordemServico);
 	}
 	
-	
+	@GetMapping("{ordemServicoId}")
+	public ResponseEntity<OrdemServico> buscar(@PathVariable Integer ordemServicoId) {
+		Optional<OrdemServico> ordemServico = ordemServicoRepository.findById(ordemServicoId);
+				
+		if(ordemServico.isPresent()) {
+			return ResponseEntity.ok().body(ordemServico.get());			
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
 }

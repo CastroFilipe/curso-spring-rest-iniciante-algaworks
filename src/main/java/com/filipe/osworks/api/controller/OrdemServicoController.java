b5698eaeb88bd4d23a4a67b1ef848242b1670524
Filipe Castro <filipe.couto.10@gmail.com>
2020-05-07 00:21:02 -0300

    listando ordens de serviço

--- src/main/java/com/filipe/osworks/api/controller/OrdemServicoController.java
+++ src/main/java/com/filipe/osworks/api/controller/OrdemServicoController.java
@@ -1,9 +1,12 @@
 package com.filipe.osworks.api.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.filipe.osworks.domain.model.OrdemServico;
+import com.filipe.osworks.domain.repository.OrdemServicoRepository;
 import com.filipe.osworks.domain.service.OrdemServicoGestaoService;
 
 @RestController
@@ -20,9 +24,19 @@ public class OrdemServicoController {
 	@Autowired
 	private OrdemServicoGestaoService ordemServicoGestaoService;
 	
+	@Autowired
+	private OrdemServicoRepository ordemServicoRepository;
+	
+	@GetMapping
+	public List<OrdemServico> listar(){
+		return ordemServicoRepository.findAll();
+	}
+	
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public OrdemServico criar(@Valid @RequestBody OrdemServico ordemServico) {
 		return ordemServicoGestaoService.criar(ordemServico);
 	}
+	
+	
 }
---
+++ src/main/resources/db/migration/V2020050618__alterar_tipo_dado_preco.sql
@@ -0,0 +1,1 @@
+ALTER TABLE ordem_servico ALTER COLUMN nu_preco SET DATA TYPE numeric(12,2);
